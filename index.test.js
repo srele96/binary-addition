e5b8f8688b8de0632a0aa9417574aeefd6f4f427
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2022-06-10 21:56:36 +0200

    Name result arrays for clarity

--- index.test.js
+++ index.test.js
@@ -23,14 +23,21 @@ test('addBinary correctly adds two binary arrays of the same length', () => {
   const two = [0, 0, 0, 1];
   const three = [0, 0, 1, 1];
   const fifteen = [1, 1, 1, 1];
-  expect(addBinary(eight, two)).toEqual([0, 1, 0, 0, 1]);
-  expect(addBinary(eight, three)).toEqual([0, 1, 0, 1, 1]);
-  expect(addBinary(eight, eight)).toEqual([1, 0, 0, 0, 0]);
-  expect(addBinary(fifteen, fifteen)).toEqual([1, 1, 1, 1, 0]);
+
+  const ten = [0, 1, 0, 0, 1];
+  const eleven = [0, 1, 0, 1, 1];
+  const sixteen = [1, 0, 0, 0, 0];
+  const thirty = [1, 1, 1, 1, 0];
+
+  expect(addBinary(eight, two)).toEqual(ten);
+  expect(addBinary(eight, three)).toEqual(eleven);
+  expect(addBinary(eight, eight)).toEqual(sixteen);
+  expect(addBinary(fifteen, fifteen)).toEqual(thirty);
 });
 
 test("addBinary doesn't return incorrect results", () => {
   const eight = [1, 0, 0, 0];
   const two = [0, 0, 0, 1];
+
   expect(addBinary(eight, two)).not.toEqual([0, 1, 0, 1, 1]);
 });
